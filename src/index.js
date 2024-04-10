eea447d79c25cdf2b5a2d5c049bc7c2b6e00db69
error <error@error-unlimited.net>
2024-04-10 13:53:58 -0500

    change ball color

--- src/index.js
+++ src/index.js
@@ -23,8 +23,10 @@ function render( t ) {
 	c2d.beginPath();
 	c2d.arc( canvas.width * .5 + ball.position.x, canvas.height * .5 - ball.position.y, ball.radius, 0, Math.PI * 2 );
 	c2d.closePath();
-	c2d.fillStyle = 'white';
+	c2d.fillStyle = 'silver';
 	c2d.fill();
+	c2d.strokeStyle = 'white';
+	c2d.stroke();
 	ball.position.x = ball.position.x + Math.cos(ball.velocity.direction) * ball.velocity.magnitude;
 	ball.position.y = ball.position.y + Math.sin(ball.velocity.direction) * ball.velocity.magnitude;
 	ball.velocity.direction = ball.velocity.direction + 0.01;
